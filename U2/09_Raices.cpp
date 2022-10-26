fbf00c6d4be1470daf31d715db97322d34bb6fee
UP210913 <up210913@alumnos.upa.edu.mx>
2022-10-26 10:21:50 -0500

    Update

---
+++ U2/09_Raices.cpp
@@ -0,0 +1,14 @@
+/*
+Date: 17/10/2022
+Author: Angel oswaldo Vargas Rocha
+Email: up210913@alumnos.upa.edu.mx
+Description: 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
